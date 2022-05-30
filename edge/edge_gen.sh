12ac0a7361477bcadc483314d2e2bbf353be862d
Butiaev Aleksei <alexsyreserv@mail.ru>
2022-05-30 22:24:17 +0300

    improve edge_gen.sh

--- edge/edge_gen.sh
+++ edge/edge_gen.sh
@@ -27,6 +27,9 @@ XB=`echo "-2.54 * $((${COUNT} / 2)) - 1.35" | bc`
 XC=`echo "2.54 * $((${COUNT} / 2)) + 1.35" | bc`
 XD=`echo "2.54 * $((${COUNT} / 2)) + 3.28" | bc`
 
+XXA=`echo "-2.54 * $((${COUNT} / 2)) - 3.6" | bc`
+XXB=`echo "2.54 * $((${COUNT} / 2)) + 3.6" | bc`
+
 #echo "COUNT=${COUNT} TEDIT=${TEDIT}  SHIFT=${SHIFT} EMPTY=${EMPTY} FROM=${FROM}"
 
 echo "(module EDGE-${COUNT}-2.54 (layer F.Cu) (tedit ${TEDIT})"
@@ -39,9 +42,16 @@ echo "  (fp_line (start ${XC} -3.75) (end ${XC} 4.65) (layer Edge.Cuts) (width 0
 echo "  (fp_line (start ${XC} 4.65) (end ${XD} 4.65) (layer Edge.Cuts) (width 0.2))"
 echo "  (fp_line (start ${XD} 4.65) (end ${XD} -3.75) (layer Edge.Cuts) (width 0.2))"
 
+for L in B.CrtYd F.CrtYd ; do
+  echo "  (fp_line (start ${XXB} 5) (end ${XXA} 5) (layer ${L}) (width 0.12))"
+  echo "  (fp_line (start ${XXA} 5) (end ${XXA} -4) (layer ${L}) (width 0.12))"
+  echo "  (fp_line (start ${XXA} -4) (end ${XXB} -4) (layer ${L}) (width 0.12))"
+  echo "  (fp_line (start ${XXB} -4) (end ${XXB} 5) (layer ${L}) (width 0.12))"
+done
+
 
 for((I=1; I<=${COUNT}; I++)); do
-  echo "  (pad A${I} smd roundrect (at ${FROM} 0) (size 1.8 6.5) (layers F.Cu F.Paste F.Mask) (roundrect_rratio 0.1) (solder_mask_margin 0.1))"
+  echo "  (pad A${I} smd roundrect (at ${FROM} 0) (size 1.8 6.5) (layers B.Cu B.Paste B.Mask) (roundrect_rratio 0.1) (solder_mask_margin 0.1))"
   echo "  (pad B${I} smd roundrect (at ${FROM} 0) (size 1.8 6.5) (layers F.Cu F.Paste F.Mask) (roundrect_rratio 0.1) (solder_mask_margin 0.1))"
   FROM=`echo "${FROM} + 2.54" | bc`
 done    
